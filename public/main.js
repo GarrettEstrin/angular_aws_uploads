3f0b6445587038b78508cac1af929bea274dd9b3
Garrett Estrin <garrett.estrin@gmail.com>
2016-11-28 15:25:57 -0800

    uploads work

--- public/main.js
+++ public/main.js
@@ -3,5 +3,65 @@ var uploadApp = angular.module('uploadApp', [])
 // awsUploadsController.$inject = []
 
 function awsUploadsController(){
+  // log to insure controller is being loaded
   console.log('awsUploadsController instantiated');
+
+  var vm = this
+
+
+      // Function to carry out the actual PUT request to S3 using the signed request from the app.
+
+   // If there is a file selected, then start upload procedure by asking for a signed request from the app.
+
+      vm.initUpload = function(){
+        var files = document.getElementById('file-input').files;
+        // var file = files[0];
+        file = files[0];
+        if(file == null){
+          return alert('No file selected.');
+        }
+        vm.getSignedRequest(file);
+      }
+
+
+        // Function to get the temporary signed request from the app.
+        // If request successful, continue to upload the file using this signed request.
+
+      vm.getSignedRequest = function (file){
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+        xhr.onreadystatechange = function(){
+          if(xhr.readyState === 4){
+            if(xhr.status === 200){
+              const response = JSON.parse(xhr.responseText);
+              // Log of response from Bucket
+              console.log("response:");
+              console.log(response);
+              vm.uploadFile(file, response.signedRequest, response.url);
+            }
+            else{
+              alert('Could not get signed URL.');
+            }
+          }
+        };
+        xhr.send();
+      }
+
+
+    vm.uploadFile = function (file, signedRequest, url){
+      const xhr = new XMLHttpRequest();
+      xhr.open('PUT', signedRequest);
+      xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+          if(xhr.status === 200){
+            document.getElementById('preview').src = url;
+          }
+          else{
+            alert('Could not upload file.');
+          }
+        }
+      };
+      xhr.send(file);
+    }
+
 }
--- server.js
+++ server.js
@@ -20,6 +20,41 @@ const
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// AWS S3
+const S3_BUCKET = process.env.S3_BUCKET;
+/*
+ * Respond to GET requests to /sign-s3.
+ * Upon request, return JSON containing the temporarily-signed S3 request and
+ * the anticipated URL of the image.
+ */
+app.get('/sign-s3', (req, res) => {
+  console.log("sign-s3 hit");
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    res.write(JSON.stringify(returnData));
+    res.end();
+  });
+});
+
+
 // Command to listen on specified port for requests
 app.listen(PORT, function() {
   console.log("Listening for requests on port:", PORT)
